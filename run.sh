b036c3fcf3af2956ec3820e5b1ad2093327c17e4
Andy Heels <AndyHeels@gmail.com>
2023-12-05 09:50:56 +0000

    Updating run.sh to zip text files

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@ echo "Running from github"
 sleep 2
 echo "I slept for 2 seconds"
 echo "Webhook added and it's amazing"
+echo "Hello from the Jenkins job named: ${JOB_NAME}"
+touch 1.txt 2.txt 3.txt 4.txt 5.txt
+zip archive.zip *.txt
 exit
